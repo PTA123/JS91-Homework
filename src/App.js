f5936c71c50513b41bbfb271884f0f36b97c346c
PTA123 <anphanvip09@gmail.com>
2024-01-05 14:35:17 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -4,59 +4,23 @@ import Profile from './components/Profile';
 import NameCard from'./components/NameCard';
 import Counter from './components/Couter';
 import Student from './components/Student';
-import Login from "./components/LoginForm";
-import Hobbies from "./components/Hobbies";
-import { useState } from 'react';
-
+import UserForm from './components/UserForm';
+import TotalUser from './components/TotalUser';
 function App() {
-  const [users, setUsers] = useState([
-    {name:"Alice", age:19, gender:"female"},
-    {name:"John", age:20, gender:"male"},
-    {name:"Cris", age:21, gender:"male"},
-  ]);
-
-  const renderNameCard = () => {
-    return users.map(user) => (
-
-    )
-  };
-
-  let button = null;
-  if(isLoggedIn) {
-    button = (
-    <button onClick={() =>{
-      setIsLoggedIn(false);
-    }}>
-        Logout
-    </button>
-    );
-  }else {
-    button = 
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <NameCard />
-
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <Profile/> */}
+        {/* <Counter/> */}
+        <NameCard name="Bob" age={30} gender="male" />
+        <NameCard name="Alice" age={25} gender="female" />
+        {/* <Student/> */}
+        {/* <UserForm/>
+        <TotalUser/> */}
       </header>
     </div>
-  )
+  );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         {/* <Profile/> */}
-//         {/* <Counter/>
-//         <NameCard name="Bob" age={30} gender="male" />
-//         <NameCard name="Alice" age={25} gender="female" /> */}
-//         <Student/>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
